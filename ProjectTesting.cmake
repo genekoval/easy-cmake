fd4d2b4162a9aa18111a916be131427a5c346ee2
Gene Koval <genyaremchuk@protonmail.com>
2023-12-10 22:34:13 -0500

    Set a PROJECT_TESTING variable

--- ProjectTesting.cmake
+++ ProjectTesting.cmake
@@ -1,7 +1,9 @@
+set(PROJECT_TESTING "")
+
 if(PROJECT_IS_TOP_LEVEL OR ${PROJECT_NAME}_TESTING)
     include(CTest)
 
     if(BUILD_TESTING)
-        set(${PROJECT_NAME}_TESTING ON)
+        set(PROJECT_TESTING ON)
     endif()
 endif()
